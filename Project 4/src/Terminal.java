290322d68b343d0682ac37a51e792972588ca572
Maddox <mbguthrie1@crimson.ua.edu>
2023-11-03 11:06:57 -0500

    Corrected error in build file, add terminal class

---
+++ Project 4/src/Terminal.java
@@ -0,0 +1,11 @@
+package Project4;
+public class Terminal {
+    public static void main(String[] args) {
+        System.out.println("The project ran!");
+    }
+
+    private void turnOff() {
+        System.exit(0);
+    }
+
+}
