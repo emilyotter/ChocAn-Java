ff7ebcd1dbbb1fa52de1a746e00c80be91cd9d56
Maddox <mbguthrie1@crimson.ua.edu>
2023-11-03 11:43:12 -0500

    Small changes to terminal class, removed .src.txt

--- Project 4/src/Terminal.java
+++ Project 4/src/Terminal.java
@@ -1,10 +1,14 @@
+// Maddox Guthrie 11/3/2023
+
 package Project4;
 public class Terminal {
     public static void main(String[] args) {
-        System.out.println("The project ran!");
+        System.out.println("Welcome to the Chocoholics Anonymous system terminal.");
+        turnOff();
     }
 
-    private void turnOff() {
+    private static void turnOff() {
+        System.out.println("Exiting the system, Thank you for choosing Chocoholics Anonymous.");
         System.exit(0);
     }
 
