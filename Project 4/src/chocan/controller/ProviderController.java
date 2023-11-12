b142b89f347680a27c92c897f06bf847cacfa3a7
etrahant <etrahant1@gmail.com>
2023-11-12 12:03:14 -0600

    method skeletons of provider controller

--- Project 4/src/chocan/controller/ProviderController.java
+++ Project 4/src/chocan/controller/ProviderController.java
@@ -8,4 +8,28 @@ public class ProviderController extends AbstractController{
     public ProviderController(CredentialsDatabase userDatabase) {
         super(userDatabase);
     }
+    
+    public void validateMember() {
+    	
+    }
+    
+    public void billChochAn() {
+    	
+    }
+    
+    public void requestProviderDirectory() {
+    	
+    }
+    
+    public void searchCode() {
+    	
+    }
+    
+    public void searchServiceFee() {
+    	
+    }
+    
+    public void emailProvider() {
+    	
+    }
 }
