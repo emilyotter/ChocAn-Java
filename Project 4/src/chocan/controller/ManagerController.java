a09e5d9e84ec05fd676e81c9124a0c11674e0f0d
emiasho <eaotter@crimson.ua.edu>
2023-11-12 12:31:55 -0600

    start of manager controller

--- Project 4/src/chocan/controller/ManagerController.java
+++ Project 4/src/chocan/controller/ManagerController.java
@@ -1,11 +1,26 @@
 package chocan.controller;
 
-
 import chocan.database.CredentialsDatabase;
 
 public class ManagerController extends AbstractController {
 
     public ManagerController(CredentialsDatabase userDatabase) {
         super(userDatabase);
+    }
+    
+    public void requestProviderReport() { 
+    	//call generate report for specified provider
+    }
+    
+    public void requestMemberReport() {
+    	//call generate report for specified member
+    }
+    
+    public void requestSummaryReport() {
+    	//call generate summary report
+    }
+    
+    public void logOut() {
+    	// return to main menu
     }
 }
