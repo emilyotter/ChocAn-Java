72328dba0fba3d85ed02e017dd8a4c0ad0785859
emiasho <eaotter@crimson.ua.edu>
2023-11-13 22:30:31 -0600

    summaryreport corrected filepath+constructer

--- Project 4/src/chocan/controller/SummaryReportController.java
+++ Project 4/src/chocan/controller/SummaryReportController.java
@@ -1,14 +1,42 @@
 package chocan.controller;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.HashMap;
 
-public class SummaryReportController extends AbstractReportController { //may not need inheritance
+import chocan.database.CredentialsDatabase;
+import chocan.database.ServiceDatabase;
 
-    public static void generateSummaryReport(ServiceDatabase serviceDatabase, String filePath) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+public class SummaryReportController  { //may not need inheritance
+
+	private ServiceDatabase serviceDatabase;
+
+    public SummaryReportController(ServiceDatabase serviceDatabase) {
+    	this.serviceDatabase = serviceDatabase;
+    }
+	
+	public void generateSummaryReport() {
+		
+		Path filePath = Path.of(System.getProperty("java.io.tmpdir")).resolve("chocan").resolve("reports");
+        
+        
+        // Create the directory if it doesn't exist
+        if (!filePath.toFile().exists()) {
+            filePath.toFile().mkdirs();
+        }
+
+        // Create the file named 
+        filePath = filePath.resolve("summary_report.txt");
+
+        // Get a file object from the path
+        File file = filePath.toFile();
+        
+        
+        // Use try-with-resources to ensure the writer is closed properly
+		try (FileWriter fstream = new FileWriter(file); BufferedWriter writer = new BufferedWriter(fstream)){
+            //BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
 
             writer.write("Provider\tConsultations\tTotal Fee");
             writer.newLine();
@@ -35,13 +63,15 @@ public class SummaryReportController extends AbstractReportController { //may no
         }
     }
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
 
         ServiceDatabase db = new ServiceDatabase();
 
-        String filePath = "summary_report.txt";
+        //String filePath = "summary_report.txt";
+        
+        SummaryReportController summaryReportController = new SummaryReportController(db);
 
-        generateSummaryReport(db, filePath);
+        summaryReportController.generateSummaryReport();
     }
 }
 
